cab704bb9f5e7ed48d8114f2136d107975d41d60
Yemelia <YemelianovN@tut.by>
2021-04-27 18:25:48 +0300

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,58 @@
-import React from 'react';
+import { useEffect, useRef, useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import * as esbuild from 'esbuild-wasm';
+
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+
+const App = () => {
+  const service = useRef<any>();
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const startService = async () => {
+    service.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm'
+    });
+  };
+
+  const onClick = async () => {
+    if (!service.current) {
+      return;
+    }
+
+    const result = await service.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+    });
+
+    setCode(result.outputFiles[0].text);
+  }
+
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={e => setInput(e.target.value)}
+      />
+      <div>
+        <button onClick={onClick}>
+          Submit
+        </button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <App />,
+  document.querySelector('#root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
